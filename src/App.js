d7338fa091f5510fd6452d3d3f40027f10a45ae8
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-09-11 12:26:53 +0200

    feat: first route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+
+const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+
 function App() {
-  return <div></div>;
+  return (
+    <RouterProvider router={router}>
+      <h1>My React App</h1>
+    </RouterProvider>
+  );
 }
 
 export default App;

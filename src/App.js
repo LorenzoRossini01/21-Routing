7c6fecca21404f80dd11bbd1cba9b0f668ae27d4
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-09-11 13:02:42 +0200

    feat: use Root route to get a reusable layout for all the routes, add an ErrorPage route

--- src/App.js
+++ src/App.js
@@ -6,19 +6,29 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
-// const router = createBrowserRouter([
-//   { path: "/", element: <HomePage /> },
-//   { path: "/products", element: <ProductsPage /> },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <ErrorPage />,
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
+]);
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
-const router = createBrowserRouter(routeDefinitions);
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<RootLayout />} children=[] />
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+// const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,9 @@
 const ProductsPage = () => {
-  return <h1>Product Page</h1>;
+  return (
+    <>
+      <h1>Product Page</h1>
+    </>
+  );
 };
 
 export default ProductsPage;

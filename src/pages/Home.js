cb034183bd05acc2e5c236aed42c2b89905b1f00
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-09-11 13:12:04 +0200

    feat: use useNavigate to navigate programmatically

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/sadad");
+  }
   return (
     <>
       <h1>Home Page</h1>
+      <button onClick={navigateHandler}>See Error Page</button>
     </>
   );
 };
